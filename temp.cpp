6661449fe0990112544497dec06011dfbaa3bd10
subsir <tiancaizhangdaxian@sjtu.edu.cn>
2023-10-06 16:00:14 +0800

    cmake installed

--- temp.cpp
+++ temp.cpp
@@ -1,59 +1,18 @@
 #include <iostream>
-#include <cstring>
+#include <cmath>
 using namespace std;
-int main(){
-    string x,y;
-    cin>>x>>y;
-    int t=0;
-    string s="";
-    int xx=x[3]-'0';
-    int yy=y[3]-'0';
-    int ss=(xx+yy)%2;
-    if(t==1){
-        if(ss==1)ss=0;
-        else {
-            ss=1;
-            t=0;
-        }
-    }
-    if(x[3]==y[3] and x[3]=='1')t=1;
-    s=(char)(ss+'0')+s;
-    xx=x[2]-'0';
-    yy=y[2]-'0';
-    ss=(xx+yy)%2;
-    if(t==1){
-        if(ss==1)ss=0;
-        else {
-            ss=1;
-            t=0;
-        }
-    }
-    if(x[2]==y[2] and x[2]=='1')t=1;
-    s=(char)(ss+'0')+s;
 
-    xx=x[1]-'0';
-    yy=y[1]-'0';
-    ss=(xx+yy)%2;
-    if(t==1){
-        if(ss==1)ss=0;
-        else {
-            ss=1;
-            t=0;
-        }
+long long order(int x) {
+    long long u=1;
+    for(int i=1;i<=x;i++) {
+        u*=i;
     }
-    if(x[1]==y[1] and x[1]=='1')t=1;
-    s=(char)(ss+'0')+s;
-    xx=x[0]-'0';
-    yy=y[0]-'0';
-    ss=(xx+yy)%2;
-    if(t==1){
-        if(ss==1)ss=0;
-        else {
-            ss=1;
-            t=0;
-        }
-    }
-    if(x[0]==y[0] and x[0]=='1')t=1;
-    s=(char)(ss+'0')+s;
-    cout<<s;
+    return u;
+}
+int main() {
+    int n;
+    cin>>n;
+    float b = order(n) * exp(1.0);
+    cout<<b- (int)(b)<<endl;
+    return main();
 }
