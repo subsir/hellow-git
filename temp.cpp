ce11df9866f8ab24bf2c0fc372173339080b9c2f
subsir <tiancaizhangdaxian@sjtu.edu.cn>
2023-10-06 19:51:38 +0800

    rebirth,congratulates!

--- temp.cpp
+++ temp.cpp
@@ -1,18 +1,8 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-
-long long order(int x) {
-    long long u=1;
-    for(int i=1;i<=x;i++) {
-        u*=i;
-    }
-    return u;
-}
 int main() {
-    int n;
-    cin>>n;
-    float b = order(n) * exp(1.0);
-    cout<<b- (int)(b)<<endl;
-    return main();
+    int a;
+    cin >> a;
+    cout << a *3 << endl;
+    return 0;
 }
